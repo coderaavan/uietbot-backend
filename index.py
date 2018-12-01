15436123d035a480a462064c9d364371b85e3f97
coderaavan <prashant.ravi812@gmail.com>
2018-12-01 20:03:44 +0530

    2nd change

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ import requests
 import json
 from support import keyWordExtractor
 app = Flask(__name__, static_folder="static")
+s = 'a'
 
 @app.route('/')
 def index():
@@ -10,6 +11,7 @@ def index():
 
 @app.route('/response', methods=['GET','POST'])
 def resp():
+    headers={'Authorization':s}
     if request.method=='POST':
         string=request.form['q']
         keywords=dict()
@@ -25,20 +27,81 @@ def resp():
         print query_data
         return render_template('result.html', qd=query_data)
 
-@app.route('/signup', methods=['POST'])
+@app.route('/signup', methods=['POST','GET'])
 def signup():
+    headers={'Authorization': s}
     if request.method=='POST':
         signupdetail={}
         signupdetail["name"]=request.form['name']
         signupdetail["username"]=request.form['username']
-        signupdetail["password"]=request.form['psw']
-        d=json.dumps(signupdetail)
-        r=requests.post("https://uiet-bot.glitch.me/users/register",json=d)
+        signupdetail["password"]=request.form['password']
+        r=requests.post("https://uiet-bot.glitch.me/users/register",json=signupdetail, headers=headers)
         print r.status_code
         if r.status_code=="200" or r.status_code==200:
             return "Signup Succesful!"
         else:
             return "Some Error"
+
+    elif request.method=='GET':
+        return render_template('signupview.html')
+
+
+@app.route('/login', methods=['POST','GET'])
+def login():
+    headers={'Authorization':s}
+    if request.method=='POST':
+        logindetail={}
+        logindetail["username"]=request.form['username']
+        logindetail["password"]=request.form['password']
+        r=requests.post("https://uiet-bot.glitch.me/users/login",json=logindetail, headers=headers)
+        print r.status_code
+        if r.status_code=="200" or r.status_code==200:
+            global s 
+            s=json.loads(r.text)['token']
+            return render_template('dashboard.html')
+        else:
+            return render_template('loginview.html',cond=True)
+
+    elif request.method=='GET':
+        return render_template('loginview.html',cond=False)
+
+@app.route('/uploadAsgn', methods=['POST'])
+def uploadAsgn():
+    headers={'Authorization':s}
+    if request.method=="POST":
+        asgndetails={}
+        asgndetails["title"]=request.form['title']
+        asgndetails["branch"]=request.form['branch']
+        asgndetails["semester"]=request.form['semester']
+        asgndetails["section"]=request.form['section']
+        asgndetails["details"]=request.form['details']
+        asgndetails["date"]=request.form['date']
+        r=requests.post("https://uiet-bot.glitch.me/assignments/put",json=asgndetails,headers=headers)
+        if r.status_code==200 or r.status_code=="200":
+            return render_template('dashboard.html')
+        else:
+            return render_template('dashboard.html')
+
+
+@app.route('/uploadExam', methods=['POST'])
+def uploadExam():
+    headers={'Authorization':s}
+    if request.method=="POST":
+        examdetails={}
+        examdetails["title"]=request.form['title']
+        examdetails["branch"]=request.form['branch']
+        examdetails["semester"]=request.form['semester']
+        examdetails["subject"]=request.form['subject']
+        examdetails["date"]=request.form['date']
+        r=requests.post("https://uiet-bot.glitch.me/exams/put",json=asgndetails,headers=headers)
+        if r.status_code==200 or r.status_code=="200":
+            return render_template('dashboard.html')
+        else:
+            return render_template('dashboard.html')
+
+
+
+
 
 if __name__=='__main__':
     app.run(debug=True)
